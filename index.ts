51a2e966e5a3f7e2c8f800a8056d081466b64361
namelos <namelos@outlook.com>
2019-03-18 16:02:19 +0800

    add vue

--- index.ts
+++ index.ts
@@ -3,5 +3,13 @@ import 'core-js/es7/reflect'
 import 'zone.js/dist/zone'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './ng/app.module'
+import Vue from 'vue'
+// @ts-ignore
+import App from './vue/App'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
---
+++ vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'vue'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
